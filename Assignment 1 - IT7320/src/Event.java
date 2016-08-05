dff7fa41653ce1c0b7b2277e6236e720011a77a2
Daniel Paterson <Daniel Paterson@192.168.1.8>
2016-08-05 17:18:56 +1200

    Added informant() and dispatcher()

--- Assignment 1 - IT7320/src/Event.java
+++ Assignment 1 - IT7320/src/Event.java
@@ -15,4 +15,15 @@ public class Event {
 	{
 		System.out.println("The event happened at: " + eventTime);
 	}
+	
+	public void informant(String informantName)
+	{
+		System.out.println("The informant's name is: " + informantName);
+	}
+	
+	public void dispatcher(String dispatcherName, String qid)
+	{
+		System.out.println("The dispatcher's is: " + dispatcherName + "\nDispatcher's QID is: " + qid);
+	}
+
 }
--- Assignment 1 - IT7320/src/MainEvent.java
+++ Assignment 1 - IT7320/src/MainEvent.java
@@ -7,20 +7,25 @@ public class MainEvent {
 		Scanner eventObj = new Scanner(System.in);
 		
 		System.out.println("Enter event type: ");
-		
 		String eventName = eventObj.nextLine();
 		
 		System.out.println("Enter event description: ");
-		
 		String eventHeadline = eventObj.nextLine();
 		
 		System.out.println("Enter location of event: ");
-		
 		String location = eventObj.nextLine();
 		
 		System.out.println("When did this event take place? ");
-		
 		String eventTime = eventObj.nextLine();
+		
+		System.out.println("Enter name of informant: ");
+		String informantName = eventObj.nextLine();
+		
+		System.out.println("Enter name of dispatcher: ");
+		String dispatcherName = eventObj.nextLine();
+		
+		System.out.println("Enter dispatcher's QID: ");
+		String qid = eventObj.nextLine();
 		
 		Event e = new Event(); 
 		
@@ -29,6 +34,10 @@ public class MainEvent {
 		e.dispatch(location);
 		
 		e.time(eventTime);
+			
+		e.informant(informantName);
+		
+		e.dispatcher(dispatcherName, qid);
 		
 		
 
